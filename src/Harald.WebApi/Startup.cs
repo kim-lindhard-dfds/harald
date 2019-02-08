db15a8c8b911fe86c88426e410ade33500013917
Thomas Wittrup Fischer <thfis@dfds.com>
2019-02-08 21:31:36 +0100

    add /healthz and /metrics endpoints

--- src/Harald.WebApi/Startup.cs
+++ src/Harald.WebApi/Startup.cs
@@ -1,16 +1,17 @@
 ﻿using System;
-using System.Linq;
-using System.Threading;
 using System.Threading.Tasks;
 using Harald.WebApi.Domain;
 using Harald.WebApi.Infrastructure.Persistence;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Prometheus;
 
 namespace Harald.WebApi
 {
@@ -28,17 +29,20 @@ namespace Harald.WebApi
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
+            var connectionString = Configuration["HARALD_DATABASE_CONNECTIONSTRING"];
+
+            Console.WriteLine(connectionString);
+            
             services
                 .AddEntityFrameworkNpgsql()
-                .AddDbContext<HaraldDbContext>((serviceProvider, options) =>
-                {
-                    var connectionString = Configuration["HARALD_DATABASE_CONNECTIONSTRING"];
-                    options.UseNpgsql(connectionString);
-                });
+                .AddDbContext<HaraldDbContext>((serviceProvider, options) => { options.UseNpgsql(connectionString); });
 
             services.AddTransient<ICapabilityRepository, CapabilityRepository>();
 
-            services.AddHealthChecks().Add(new HealthCheckRegistration("DatabaseAlive", sp => new DatabaseHealthCheck(sp.GetRequiredService<ICapabilityRepository>()), HealthStatus.Unhealthy, Enumerable.Empty<string>()));
+            services.AddHealthChecks()
+                .AddCheck("self", () => HealthCheckResult.Healthy())
+                .AddNpgSql(connectionString, tags: new[] {"backing services", "postgres"})
+                ;
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -52,30 +56,56 @@ namespace Harald.WebApi
             app.UseHttpsRedirection();
             app.UseMvc();
 
-            app.UseHealthChecks("/healthz");
+            app.UseHealthChecks("/healthz", new HealthCheckOptions
+            {
+                ResponseWriter = MyPrometheusStuff.WriteResponseAsync
+
+            });
+
+            app.UseMetricServer();
         }
     }
 
-    public class DatabaseHealthCheck : IHealthCheck
+    public static class MyPrometheusStuff
     {
-        private readonly ICapabilityRepository _capabilityRepository;
+        private const string HealthCheckLabelName = "healthcheck";
 
-        public DatabaseHealthCheck(ICapabilityRepository capabilityRepository)
+        private static readonly Gauge HealthChecksDuration;
+        private static readonly Gauge HealthChecksResult;
+
+        static MyPrometheusStuff()
         {
-            _capabilityRepository = capabilityRepository;
+            HealthChecksResult = Metrics.CreateGauge("healthcheck",
+                "Shows raw health check status (0 = Unhealthy, 1 = Degraded, 2 = Healthy)", new GaugeConfiguration
+                {
+                    LabelNames = new[] {HealthCheckLabelName},
+                    SuppressInitialValue = false
+                });
+
+            HealthChecksDuration = Metrics.CreateGauge("healthcheck_duration_seconds",
+                "Shows duration of the health check execution in seconds",
+                new GaugeConfiguration
+                {
+                    LabelNames = new[] {HealthCheckLabelName},
+                    SuppressInitialValue = false
+                });
         }
 
-        public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = new CancellationToken())
+        public static Task WriteResponseAsync(HttpContext httpContext, HealthReport healthReport)
         {
-            try
-            {
-                await _capabilityRepository.Get(Guid.Empty);
+            UpdateMetrics(healthReport);
 
-                return HealthCheckResult.Healthy();
-            }
-            catch 
+            httpContext.Response.ContentType = "text/plain";
+            return httpContext.Response.WriteAsync(healthReport.Status.ToString());
+        }
+
+        private static void UpdateMetrics(HealthReport report)
+        {
+            foreach (var (key, value) in report.Entries)
             {
-                return HealthCheckResult.Unhealthy();
+                HealthChecksResult.Labels(key).Set((double) value.Status);
+
+                HealthChecksDuration.Labels(key).Set(value.Duration.TotalSeconds);
             }
         }
     }
