51defd01b6d040f28a9414f4c59b08c8a2a21445
Jakob Nielsen <jakob.nielsen@dfds.com>
2019-02-22 10:47:10 +0100

    Improved fixing Slack channel and handle names.

--- src/Harald.WebApi/Infrastructure/Facades/Slack/SlackFacade.cs
+++ src/Harald.WebApi/Infrastructure/Facades/Slack/SlackFacade.cs
@@ -162,16 +162,9 @@ namespace Harald.WebApi.Infrastructure.Facades.Slack
                 channelName = channelName.Substring(0, 21);
             }
 
-            // Make sure at least one word is matched below (starts with upper-case character).
-            channelName = UppercaseFirstCharacter(channelName);
+            var fixedChannelName = GetLowerCaseNameWithHypens(channelName);
 
-            var lowerCaseWords = Regex.Matches(channelName, @"([A-Z][a-z]+)")
-            .Cast<Match>()
-            .Select(m => m.Value.ToLower());
-
-            var lowerCaseChannelNameWithHypens = string.Join("-", lowerCaseWords);
-
-            return lowerCaseChannelNameWithHypens;
+            return fixedChannelName;
         }
 
         private string FixHandleNameForSlack(string handleName)
@@ -179,16 +172,40 @@ namespace Harald.WebApi.Infrastructure.Facades.Slack
             const string handleSuffix = "Members";
             handleName = handleName + handleSuffix;
 
-            // Make sure at least one word is matched below (starts with upper-case character).
-            handleName = UppercaseFirstCharacter(handleName);
+            var fixedHandleName = GetLowerCaseNameWithHypens(handleName);
 
-            var lowerCaseWords = Regex.Matches(handleName, @"([A-Z][a-z]+)")
+            return fixedHandleName;
+        }
+
+        private string GetLowerCaseNameWithHypens(string name)
+        {
+            // Make sure at least one word is matched below (starts with upper-case character).
+            name = UppercaseFirstCharacter(name);
+
+            var regExPattern = string.Empty;
+
+            // Only dash is allowed for channel/handle names, replace underscore with dash.
+            if (name.Contains("_"))
+            {
+                name = name.Replace("_", "-");
+            }
+
+            if (name.Contains("-"))
+            {
+                regExPattern = @"[a-zA-Z1-9]+-[a-zA-Z1-9]+";
+            }
+            else
+            {
+                regExPattern = @"([A-Z1-9][a-z1-9]+)";
+            }
+
+            var lowerCaseWords = Regex.Matches(name, regExPattern)
             .Cast<Match>()
             .Select(m => m.Value.ToLower());
 
-            var lowerCaseHandleNameWithHypens = string.Join("-", lowerCaseWords);
+            var lowerCaseNameWithHypens = string.Join("-", lowerCaseWords);
 
-            return lowerCaseHandleNameWithHypens;
+            return lowerCaseNameWithHypens;
         }
 
         private string UppercaseFirstCharacter(string s)
