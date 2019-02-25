a2ca271ae01b09f1ffcb3d99785f601ae1612db9
Jakob Nielsen <jakob.nielsen@dfds.com>
2019-02-25 13:21:13 +0100

    Hooked in repository to fetch channel ID for capability in API. Improved HTTP status codes.

--- src/Harald.WebApi/Controllers/DispatchController.cs
+++ src/Harald.WebApi/Controllers/DispatchController.cs
@@ -1,7 +1,9 @@
 using System;
 using System.ComponentModel.DataAnnotations;
 using System.Threading.Tasks;
+using Harald.WebApi.Domain;
 using Harald.WebApi.Infrastructure.Facades.Slack;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Harald.WebApi.Controllers
@@ -11,19 +13,41 @@ namespace Harald.WebApi.Controllers
     public class DispatchController : ControllerBase
     {
         private readonly ISlackFacade _slackFacade;
+        private readonly ICapabilityRepository _capabilityRepository;
 
-        public DispatchController(ISlackFacade slackFacade)
+        public DispatchController(
+            ISlackFacade slackFacade, 
+            ICapabilityRepository capabilityRepository)
         {
             _slackFacade = slackFacade;
+            _capabilityRepository = capabilityRepository;
         }
 
         [HttpPost]
         public async Task<IActionResult> DispatchMessage(DispatchMessageInput input)
         {
-            // TODO: Fetch channel from DB using capibilityId.
-            const string capabilityChannel = "ded-team-one";
-            await _slackFacade.SendNotificationToChannel(capabilityChannel, input.Message);
+            if (!input.CapabilityId.HasValue)
+            {
+                return BadRequest("capabilityId is required.");
+            }
+
+            var capability = await _capabilityRepository.Get(input.CapabilityId.Value);
+
+            if (capability == null)
+            {
+                return UnprocessableEntity($"Capability ID '{input.CapabilityId.Value}' doesn't exist.");
+            }
+
+            var sendNotificationToChannelResponse =
+             await _slackFacade.SendNotificationToChannel(capability.SlackChannelId, input.Message);
          
+            if (!sendNotificationToChannelResponse.Ok)
+            {
+                return StatusCode(
+                    StatusCodes.Status424FailedDependency,
+                    $"An error occured trying to send notification: {sendNotificationToChannelResponse.Error}");
+            }
+
             return Accepted();
         }
     }
