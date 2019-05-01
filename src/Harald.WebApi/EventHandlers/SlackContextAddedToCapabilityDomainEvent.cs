c6e2c4cbf9a7f76fa626a0d4148dc124e0890b09
Kim Lindhard <kilin@dfds.com>
2019-05-01 20:55:50 +0200

    write message in ded-aesir channel when context is added

--- src/Harald.WebApi/EventHandlers/SlackContextAddedToCapabilityDomainEvent.cs
+++ src/Harald.WebApi/EventHandlers/SlackContextAddedToCapabilityDomainEvent.cs
@@ -1,14 +1,39 @@
 using System;
+using System.Net.Http;
 using System.Threading.Tasks;
+using Harald.WebApi.Domain;
 using Harald.WebApi.Domain.Events;
+using Harald.WebApi.Infrastructure.Facades.Slack;
 
 namespace Harald.WebApi.EventHandlers
 {
     public class SlackContextAddedToCapabilityDomainEvent : IEventHandler<ContextAddedToCapabilityDomainEvent>
     {
-        public Task HandleAsync(ContextAddedToCapabilityDomainEvent domainEvent)
+        private readonly HttpClient _client;
+
+        private readonly ICapabilityRepository _capabilityRepository;
+        private readonly ISlackFacade _slackFacade;
+
+        public SlackContextAddedToCapabilityDomainEvent(ICapabilityRepository capabilityRepository, ISlackFacade slackFacade)
         {
-            return Task.CompletedTask;
+            _capabilityRepository = capabilityRepository;
+            _slackFacade = slackFacade;
+        }
+
+        public async Task HandleAsync(ContextAddedToCapabilityDomainEvent domainEvent)
+        {
+            var capability = await _capabilityRepository.Get(domainEvent.Data.CapabilityId);
+
+            var capabilityName = capability != null ? 
+                capability.Name : 
+                domainEvent.Data.CapabilityId.ToString();
+
+
+            var hardCodedDedChannelId = "GFYE9B99Q";
+
+            var message = $"context: \"{domainEvent.Data.ContextName}\" have been added to capability : \"{capabilityName}\". Have  a nice day";
+
+            _slackFacade.SendNotificationToChannel(hardCodedDedChannelId, message);
         }
     }
 }
