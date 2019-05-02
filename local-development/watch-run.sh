8fbec288d010b971ab688affcf97f2d0746e459e
Kim Lindhard <kilin@dfds.com>
2019-05-02 06:45:05 +0200

    add localdevelopment watch run script

---
+++ local-development/watch-run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+if [ -z "$SLACK_API_BASE_URL" ]
+then
+      echo "Warning: \$SLACK_API_BASE_URL is empty"
+fi
+if [ -z "$SLACK_API_AUTH_TOKEN" ]
+then
+      echo "Warning: \$SLACK_API_AUTH_TOKEN is empty"
+fi
+
+
+
+HARALD_DATABASE_CONNECTIONSTRING="User ID=postgres;Password=p;Host=localhost;Port=5432;Database=harald_db;" \
+HARALD_KAFKA_BOOTSTRAP_SERVERS=localhost:9092 \
+HARALD_KAFKA_GROUP_ID=harald-consumer \
+HARALD_KAFKA_ENABLE_AUTO_COMMIT=false \
+dotnet watch --project ./../src/Harald.WebApi/Harald.WebApi.csproj \
+run --server.urls "http://*:5123" --no-launch-profile
