61419c8c040e1e2422ac5fd42927f83cf7f7f6a2
JacobHeidelbachDFDS <jfhei@dfds.com>
2019-05-10 14:38:16 +0200

    Merged

--- src/Harald.Tests/EventHandlers/SlackAWSContextAccountCreatedEventHandlerTests.cs
+++ src/Harald.Tests/EventHandlers/SlackAWSContextAccountCreatedEventHandlerTests.cs
@@ -1,8 +1,45 @@
+
+using System.Threading.Tasks;
+using Harald.Tests.Builders;
+using Harald.Tests.TestDoubles;
+using Harald.WebApi.Domain.Events;
+using Harald.WebApi.EventHandlers;
+using Harald.WebApi.Infrastructure.Facades.Slack;
+using Xunit;
+
 namespace Harald.Tests.EventHandlers
 {
     public class SlackAWSContextAccountCreatedEventHandlerTests
     {
-        
-        
+
+        [Fact]
+        public void can_handle_domain_event()
+        {
+            var sut = new SlackAWSContextAccountCreatedEventHandler(new StubSlackFacade(false));
+            var @event = DomainEventBuilder.BuildAWSContextAccountCreatedEvent();
+
+            sut.HandleAsync(@event);
+
+        }
+    }
+
+    public class SlackAWSContextAccountCreatedEventHandler : IEventHandler<AWSContextAccountCreatedDomainEvent>
+    {
+        private readonly ISlackFacade _slackFacade;
+
+        public SlackAWSContextAccountCreatedEventHandler(ISlackFacade slackFacade)
+        {
+            _slackFacade = slackFacade;
+        }
+
+        public Task HandleAsync(AWSContextAccountCreatedDomainEvent domainEvent)
+        {
+<<<<<<< Updated upstream
+=======
+            _slackFacade.SendNotificationToChannel()
+>>>>>>> Stashed changes
+            
+            return Task.CompletedTask;
+        }
     }
 }
