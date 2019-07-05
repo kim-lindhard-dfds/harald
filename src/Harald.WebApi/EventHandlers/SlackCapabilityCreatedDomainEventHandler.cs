630834f52daa1659c3e2fddef641a2293ac108d3
Emil H. Clausen <emcla@dfds.com>
2019-07-05 09:58:41 +0200

    Fixed typo in message

--- src/Harald.WebApi/EventHandlers/SlackCapabilityCreatedDomainEventHandler.cs
+++ src/Harald.WebApi/EventHandlers/SlackCapabilityCreatedDomainEventHandler.cs
@@ -58,7 +58,7 @@ namespace Harald.WebApi.EventHandlers
                   $"Thank you for creating capability '{capability.Name}'.\n" +
                   $"This channel along with handle @{createUserGroupResponse.UserGroup.Handle} has been created.\n" + 
                   "Use the handle to notify capability members.\n" + 
-                  $"If you wan't to define a better handle, you can do this in the '{createUserGroupResponse.UserGroup.Name}'");
+                  $"If you want to define a better handle, you can do this in the '{createUserGroupResponse.UserGroup.Name}'");
 
                 // Pin message.
                 await _slackFacade.PinMessageToChannel(channelId, sendNotificationResponse.TimeStamp);
