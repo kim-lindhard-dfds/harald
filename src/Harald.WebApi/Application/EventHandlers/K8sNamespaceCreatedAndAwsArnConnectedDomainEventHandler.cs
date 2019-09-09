0bed70868ba66402e9a6e5754dc621f235a667dc
Kim Lindhard <kilin@dfds.com>
2019-09-09 05:29:54 +0200

    refactor K8SNamespaceCreatedAndAwsArnConnectedDomainEventHandler

--- src/Harald.WebApi/Application/EventHandlers/K8sNamespaceCreatedAndAwsArnConnectedDomainEventHandler.cs
+++ src/Harald.WebApi/Application/EventHandlers/K8sNamespaceCreatedAndAwsArnConnectedDomainEventHandler.cs
@@ -3,22 +3,18 @@ using System.Threading.Tasks;
 using Harald.WebApi.Domain;
 using Harald.WebApi.Domain.Events;
 using Harald.WebApi.Infrastructure.Facades.Slack;
-using Microsoft.Extensions.Logging;
 
 namespace Harald.WebApi.Application.EventHandlers
 {
     public class K8SNamespaceCreatedAndAwsArnConnectedDomainEventHandler : IEventHandler<K8sNamespaceCreatedAndAwsArnConnectedDomainEvent>
     {
-        private readonly ILogger<K8SNamespaceCreatedAndAwsArnConnectedDomainEventHandler> _logger;
         private readonly ISlackFacade _slackFacade;
         private readonly ICapabilityRepository _capabilityRepository;
 
         public K8SNamespaceCreatedAndAwsArnConnectedDomainEventHandler(
-            ILogger<K8SNamespaceCreatedAndAwsArnConnectedDomainEventHandler> logger,
             ISlackFacade slackFacade,
             ICapabilityRepository capabilityRepository)
         {
-            _logger = logger;
             _slackFacade = slackFacade;
             _capabilityRepository = capabilityRepository;
         }
@@ -26,14 +22,33 @@ namespace Harald.WebApi.Application.EventHandlers
         public async Task HandleAsync(K8sNamespaceCreatedAndAwsArnConnectedDomainEvent domainEvent)
         {
             var capability = await _capabilityRepository.Get(domainEvent.Payload.CapabilityId);
-            
+
             // 1st Message, instant.
-            await _slackFacade.SendNotificationToChannel(capability.ChannelId, $"Nearly there... time to grab a coffee?\n{SlackContextAddedToCapabilityDomainEventHandler.CreateTaskTable(true, true, false)}");
+            var missingAdsyncTaskTable = SlackContextAddedToCapabilityDomainEventHandler.CreateTaskTable(
+                awsAccDone:true, 
+                k8sCreatedDone:true, 
+                adsyncDone:false
+            );
+            
+            await _slackFacade.SendNotificationToChannel(
+                capability.ChannelId, 
+                $"Nearly there... time to grab a coffee?\n{missingAdsyncTaskTable}"
+                );
 
             var timeToWait = (60 * 15); // 15 Minutes
             var timestamp = DateTimeOffset.UtcNow.ToUnixTimeSeconds() + timeToWait;
+            
             // 2nd Message, delayed.
-            await _slackFacade.SendDelayedNotificationToChannel(capability.ChannelId, $"All done:\n{SlackContextAddedToCapabilityDomainEventHandler.CreateTaskTable(true, true, true)}", timestamp);
+            var allDoneTaskTable = SlackContextAddedToCapabilityDomainEventHandler.CreateTaskTable(
+                awsAccDone:true,
+                k8sCreatedDone: true, 
+                adsyncDone:true
+            );
+            await _slackFacade.SendDelayedNotificationToChannel(
+                capability.ChannelId, 
+                $"All done:\n{allDoneTaskTable}", 
+                timestamp
+            );
         }
     }
 }
