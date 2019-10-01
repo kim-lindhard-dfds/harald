a1927dbf90e08a45f66d01c8076f4dffc1faca36
Kim Lindhard <kilin@dfds.com>
2019-10-01 05:28:02 +0200

    slack api server spy: add channels.join & conversations.list

--- local-development/slack-api-server-spy/server.js
+++ local-development/slack-api-server-spy/server.js
@@ -5,8 +5,8 @@ const port = process.env.port || 1447;
 const app = express();
 app.use(express.json());
 
-const interactions = [];
-const conversations = [];
+var interactions = [];
+var conversations = [];
 function collectRequestData(request) {
     let interaction = {
         path: request.path,
@@ -51,8 +51,8 @@ app.get("/api/usergroups.list", (req, res) => {
 app.post("/api/channels.create", async (req, res) => {
     collectRequestData(req);
 
-    let channel = { 
-        "Id": generateRandomString(9).toUpperCase(), 
+    let channel = {
+        "Id": generateRandomString(9).toUpperCase(),
         "Name": req.body.name
     };
     conversations.push(channel);
@@ -89,10 +89,40 @@ app.post("/api/pins.add", async (req, res) => {
     });
 });
 
+app.post("/api/channels.join", async (req, res) => {
+    collectRequestData(req);
+    
+    let channel = conversations
+    .find(function (conversation) { return (conversation.name == req.body.name); });
+    
+    if(channel === undefined){ 
+        channel = {
+            "Id": generateRandomString(9).toUpperCase(),
+            "Name": req.body.name
+        };
+        conversations.push(channel);
+    }
+
+    return res.json({
+        "Ok": true,
+        "Channel": channel
+    });
+});
+
 app.post("*", async (req, res) => {
     collectRequestData(req);
 
     return res.json({ success: true });
+});
+
+
+app.get("/api/conversations.list", (req, res) => {
+    collectRequestData(req);
+
+    return res.json({
+        "Ok": true,
+        "Channels" : conversations
+    });
 });
 
 
@@ -104,12 +134,14 @@ app.get("/api/usergroups.list", (req, res) => {
     });
 });
 
+
+
 function generateRandomString(length) {
-    var result           = '';
-    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var result = '';
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     var charactersLength = characters.length;
-    for ( var i = 0; i < length; i++ ) {
-       result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    for (var i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
     }
     return result;
 }
