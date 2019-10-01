6d97f29b1ced585e7bb35276cb6acbdef448cefe
Kim Lindhard <kilin@dfds.com>
2019-10-01 05:26:49 +0200

    integrations test Add2ConnectionsToClientScenario create slack channels to join

--- integration-tests/Harald.IntegrationTests/Features/Infrastructure/Model/ConnectionDto.cs
+++ integration-tests/Harald.IntegrationTests/Features/Infrastructure/Model/ConnectionDto.cs
@@ -5,7 +5,10 @@ namespace Harald.IntegrationTests.Features.Infrastructure.Model
         public string ClientType { get; set; }
         public string ClientId { get; set; }
         public string ClientName { get; set; }
+        
+        
         public string ChannelType { get; set; }
         public string ChannelId { get; set; }
+        public string ChannelName { get; set; }
     }
 }
