e560b3a26b825e378051995c631dcd1710cca9b7
Jan Wiberg <jawib@dfds.com>
2019-10-10 09:55:14 +0200

    Blank slate

--- src/Harald.WebApi/Startup.cs
+++ src/Harald.WebApi/Startup.cs
@@ -22,7 +22,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Hosting;
-using Prometheus;
 
 namespace Harald.WebApi
 {
@@ -72,7 +71,6 @@ namespace Harald.WebApi
 
             services.AddConnectionDependencies();
             
-            services.AddHostedService<MetricHostedService>();
             services.AddHostedService<ConsumerHostedService>();
 
             services.AddHealthChecks()
@@ -138,85 +136,8 @@ namespace Harald.WebApi
             app.UseSwaggerUi3();
 
             app.UseMvc();
-
-            app.UseHealthChecks("/healthz", new HealthCheckOptions
-            {
-                ResponseWriter = MyPrometheusStuff.WriteResponseAsync
-            });
         }
     }
 
-    public class MetricHostedService : IHostedService
-    {
-        private const string Host = "0.0.0.0";
-        private const int Port = 8080;
-
-        private IMetricServer _metricServer;
-
-        public Task StartAsync(CancellationToken cancellationToken)
-        {
-            Console.WriteLine($"Staring metric server on {Host}:{Port}");
-
-            _metricServer = new KestrelMetricServer(Host, Port).Start();
-
-            return Task.CompletedTask;
-        }
-
-        public async Task StopAsync(CancellationToken cancellationToken)
-        {
-            using (_metricServer)
-            {
-                Console.WriteLine("Shutting down metric server");
-                await _metricServer.StopAsync();
-                Console.WriteLine("Done shutting down metric server");
-            }
-        }
-    }
-
-    public static class MyPrometheusStuff
-    {
-        private const string HealthCheckLabelServiceName = "service";
-        private const string HealthCheckLabelStatusName = "status";
-
-        private static readonly Gauge HealthChecksDuration;
-        private static readonly Gauge HealthChecksResult;
-
-        static MyPrometheusStuff()
-        {
-            HealthChecksResult = Metrics.CreateGauge("healthcheck",
-                "Shows health check status (status=unhealthy|degraded|healthy) 1 for triggered, otherwise 0", new GaugeConfiguration
-                {
-                    LabelNames = new[] { HealthCheckLabelServiceName, HealthCheckLabelStatusName },
-                    SuppressInitialValue = false
-                });
-
-            HealthChecksDuration = Metrics.CreateGauge("healthcheck_duration_seconds",
-                "Shows duration of the health check execution in seconds",
-                new GaugeConfiguration
-                {
-                    LabelNames = new[] { HealthCheckLabelServiceName },
-                    SuppressInitialValue = false
-                });
-        }
-
-        public static Task WriteResponseAsync(HttpContext httpContext, HealthReport healthReport)
-        {
-            UpdateMetrics(healthReport);
-
-            httpContext.Response.ContentType = "text/plain";
-            return httpContext.Response.WriteAsync(healthReport.Status.ToString());
-        }
-
-        private static void UpdateMetrics(HealthReport report)
-        {
-            foreach (var (key, value) in report.Entries)
-            {
-                HealthChecksResult.Labels(key, "healthy").Set(value.Status == HealthStatus.Healthy ? 1 : 0);
-                HealthChecksResult.Labels(key, "unhealthy").Set(value.Status == HealthStatus.Unhealthy ? 1 : 0);
-                HealthChecksResult.Labels(key, "degraded").Set(value.Status == HealthStatus.Degraded ? 1 : 0);
-
-                HealthChecksDuration.Labels(key).Set(value.Duration.TotalSeconds);
-            }
-        }
-    }
+    
 }
