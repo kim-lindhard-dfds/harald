d7dc0173e25ccabfabc5a08330a3161e21d08c90
Kim Lindhard <kilin@dfds.com>
2020-02-27 11:17:49 +0100

    Toggle metric server

--- local-development/watch-run.sh
+++ local-development/watch-run.sh
@@ -14,5 +14,6 @@ HARALD_DATABASE_CONNECTIONSTRING="User ID=postgres;Password=p;Host=localhost;Por
 HARALD_KAFKA_BOOTSTRAP_SERVERS=localhost:9092 \
 HARALD_KAFKA_GROUP_ID=harald-consumer \
 HARALD_KAFKA_ENABLE_AUTO_COMMIT=false \
+HARALD_START_METRIC_SERVER=false \
 dotnet watch --project ./../src/Harald.WebApi/Harald.WebApi.csproj \
 run --server.urls "http://*:5123" --no-launch-profile
--- src/Harald.WebApi/Enablers/Metrics/Configuration/DependencyInjection.cs
+++ src/Harald.WebApi/Enablers/Metrics/Configuration/DependencyInjection.cs
@@ -4,8 +4,10 @@ namespace Harald.WebApi.Enablers.Metrics.Configuration
 {
     public static class DependencyInjection
     {
-        public static void AddMetrics(this IServiceCollection services)
+        public static void AddMetrics(this IServiceCollection services, bool startHostedService)
         {
+            if(startHostedService == false) {return;}
+            
             services.AddHostedService<MetricHostedService>();
         }
     }
