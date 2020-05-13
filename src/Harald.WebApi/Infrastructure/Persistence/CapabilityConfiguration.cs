4bc5adb38e9f394d08b8ef1eeaeff0b0ba54e961
toban@dfds.com <toban@dfds.com>
2020-05-13 15:45:19 +0200

    - Added HasMany configuration - Added SetField to explicitly specify backing field

--- src/Harald.WebApi/Infrastructure/Persistence/CapabilityConfiguration.cs
+++ src/Harald.WebApi/Infrastructure/Persistence/CapabilityConfiguration.cs
@@ -13,7 +13,10 @@ namespace Harald.WebApi.Infrastructure.Persistence
             builder.Property(e => e.SlackChannelId)
                     .HasConversion(v => v.ToString(), v => new ChannelId(v));
 
+            builder.HasMany(typeof(CapabilityMember), "Members").WithOne();
+
             var navigation = builder.Metadata.FindNavigation(nameof(Capability.Members));
+            navigation?.SetField("_members");
             navigation?.SetPropertyAccessMode(PropertyAccessMode.Field);
         }
     }
