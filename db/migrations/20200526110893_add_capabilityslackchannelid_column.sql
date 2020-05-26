f4a0e19a333cedadec8660178d7c430f40344fdd
toban@dfds.com <toban@dfds.com>
2020-05-26 19:29:42 +0200

    - Re-introducing "CapabilitySlackChannelId" and constrain needed match Capability PKEy bindings. I will update the missing values in another migration

---
+++ db/migrations/20200526110893_add_capabilityslackchannelid_column.sql
@@ -0,0 +1,5 @@
+-- 2020-05-26 11:08:23 : remove-colum
+ALTER TABLE "CapabilityMember" 
+ADD COLUMN "CapabilitySlackChannelId" VARCHAR NULL;
+ALTER TABLE "CapabilityMember" 
+ADD CONSTRAINT "FK_CapabilityMember_CapabilityId_CapabilitySlackChannelId" FOREIGN KEY ("CapabilityId", "CapabilitySlackChannelId") REFERENCES "Capability" ("Id", "SlackChannelId") ON DELETE RESTRICT;
