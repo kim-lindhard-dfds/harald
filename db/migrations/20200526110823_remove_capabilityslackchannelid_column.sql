8bd754be6080eacdc7e6c779003dd96ac773cce6
Emil Hummel <emcla@dfds.com>
2020-05-26 12:37:11 +0200

    Update 20200526110823_remove_capabilityslackchannelid_column.sql

--- db/migrations/20200526110823_remove_capabilityslackchannelid_column.sql
+++ db/migrations/20200526110823_remove_capabilityslackchannelid_column.sql
@@ -1,4 +1,5 @@
 -- 2020-05-26 11:08:23 : remove-colum
 ALTER TABLE "CapabilityMember" 
 DROP CONSTRAINT "FK_CapabilityMember_CapabilityId_CapabilitySlackChannelId";
+ALTER TABLE "CapabilityMember"
 DROP COLUMN "CapabilitySlackChannelId";
